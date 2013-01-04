0052b3113d413191b83c48e45b5dfb1328ea2071
Priority1 <pr1@pr1.ru>
2013-01-04 20:29:37 +0400

    remove

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lmysqlclient -lcurl -lpcreposix -lfam -lconfig
-
